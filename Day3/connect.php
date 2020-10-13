d0000d3d8ced8b6571c076ebefb4cad45e7159ef
Neeraj497 <72257373+Neeraj497@users.noreply.github.com>
2020-10-13 21:36:17 +0530

    Update connect.php

--- Day3/connect.php
+++ Day3/connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "resut";
+$dbname = "result";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
